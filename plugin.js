02948752c0cd53ed70b6b2dc177e952e0008a5c8
Jérôme Nicolas Imfeld <jerome@shtill.com>
2021-07-21 01:44:19 +0200

    add fallback load fn

--- plugin.js
+++ plugin.js
@@ -7,7 +7,7 @@ export default
   ({
     name: name,
     resolve: { input, output },
-    load: load && (async x => load(x, conf)),
+    load: load ? (async x => load(x, conf)) : () => {},
     transform: transform && (async x => transform(x, conf)),
     optimize: optimize && (async x => optimize(x, conf)),
   })
