566bcecf228b17e23a28510877cca2e53f8d22cc
Jérôme Nicolas Imfeld <jerome@shtill.com>
2021-07-21 00:52:52 +0200

    update plugin

--- plugin.js
+++ plugin.js
@@ -2,17 +2,12 @@ import { name } from './package.json'
 
 
 
-export default function ( snowpackConfig, pluginOptions = {} ) {
-  return {
+export default 
+  (conf, { input, output, load, transform, optimize }) =>
+  ({
     name: name,
-
-    resolve: {
-      input: ['.css', '.js'],
-      output: ['.css', '.js'],
-    },
-
-    load: () => { return 'value' },
-    
-    optimize: () => { return 'value' }
-  };
-};
+    resolve: { input, output },
+    load: load && (async x => load(x, conf)),
+    transform: transform && (async x => transform(x, conf)),
+    optimize: optimize && (async x => optimize(x, conf)),
+  })
