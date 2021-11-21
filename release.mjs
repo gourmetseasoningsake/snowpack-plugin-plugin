4124847ba8458e9cf45f8bd4a82eaa9ab91ded76
gourmetseasoningsake <jerome@shtill.com>
2021-11-21 23:42:43 +0100

    test: release

--- release.mjs
+++ release.mjs
@@ -1,10 +1,18 @@
 import { execSync } from 'child_process'
 
 
+
+const args = process.argv.slice(2)
+const releaseType = args[0]
+
+
+
 const version = 
-  execSync('npm version patch', { encoding: 'utf8'})
+  execSync(`npm version ${releaseType}`, { encoding: 'utf8' })
   .trim()
   .substring(1)
+
+
 
 
 console.log(version)
