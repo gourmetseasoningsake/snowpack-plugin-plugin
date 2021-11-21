af210250733f617e62996ca2c587c6efd1709f05
gourmetseasoningsake <jerome@shtill.com>
2021-11-21 23:34:34 +0100

    test: release

--- release.mjs
+++ release.mjs
@@ -2,7 +2,7 @@ import pkg from './package.json'
 import { execSync } from 'child_process'
 
 
-const blub = execSync('npm version patch --json', { encoding: 'utf8'})
+const blub = execSync('npm version patch', { encoding: 'utf8'})
 
 
 //const version = execSync(`npm view ${pkg.name} version`, { encoding: 'utf8'}).trim()
