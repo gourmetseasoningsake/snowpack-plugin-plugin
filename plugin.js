6c9e6f1add459f3b7604bbe34f20cb07c3268a2e
gourmetseasoningsake <jerome@shtill.com>
2021-11-26 00:07:51 +0100

    1-refactor-plugin (#2)
    * fix(husky): test husky
    
    * test: release
    
    * 0.3.5
    
    * test: release
    
    * 0.3.6
    
    * 0.3.7
    
    * test: release
    
    * 0.3.8
    
    * test: release
    
    * 0.3.9
    
    * test: release
    
    * 0.3.10
    
    * test: release
    
    * 0.3.11
    
    * test: release
    
    * 0.3.12
    
    * test: release
    
    * 0.3.13
    
    * test: release
    
    * 0.3.14
    
    * test: release
    
    * 0.3.15
    
    * test: release
    
    * 0.3.16
    
    * test: release
    
    * 0.3.17
    
    * test: release
    
    * 0.3.18
    
    * test: release
    
    * 0.4.0
    
    * pr refactor: plugin
    
    * chore(cleanup): plugin
    
    * 1.1.0

--- plugin.js
+++ plugin.js
@@ -3,40 +3,46 @@ import { name } from './package.json'
 
 
 const isArray = x => Array.isArray(x)
-
-
-
 const isFunction = f => typeof f === 'function'
 
 
 
-export default ( 
-    unsafeConfig, {
+const plugin = ( 
+    snowpackConfig, {
     input, output,
+    config,
     load, 
     transform, 
     run, 
     optimize
   }) => {
-
+    test()
     let value = { name }
+
+    if (isFunction(config)) {
+      value.config = x => config(x)
+    }
 
     if (isArray(input) && isArray(output) && isFunction(load)) {
       value.resolve = { input, output }
-      value.load = x => load(x, unsafeConfig)
+      value.load = x => load(x, snowpackConfig)
     }
 
     if (isFunction(transform)) {
-      value.transform = x => transform(x, unsafeConfig)
+      value.transform = x => transform(x, snowpackConfig)
     }
 
     if (isFunction(run)) {
-      value.run = x => run(x, unsafeConfig)
+      value.run = x => run(x, snowpackConfig)
     }
 
     if (isFunction(optimize)) {
-      value.optimize = x => optimize(x, unsafeConfig)
+      value.optimize = x => optimize(x, snowpackConfig)
     }
 
     return value
   }
+
+
+
+export default plugin
